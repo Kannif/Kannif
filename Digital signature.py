e27cdd088a5fd7dd3937b58627050e15c94b75bc
Sunny <102226353+Kannif@users.noreply.github.com>
2022-06-02 20:39:52 +0530

    Update Digital signature.py

--- Digital signature.py
+++ Digital signature.py
@@ -1,49 +1,42 @@
-import sys
-from Crypto.PublicKey import RSA
-from Crypto.Hash import SHA256
-from Crypto.Signature import PKCS1_v1_5
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
+import java.security.Signature;
+import java.util.Scanner;
 
-def usage():
-    print("Usage: \n"
-            "digi-sig -s  <priv-key> <data> <signature-file> \n"
-            "digi-sig -v  <PUB-key> <data> <signature-file> \n")
-
-if (len(sys.argv) < 5):
-    usage()
-    quit()
-
-op = sys.argv[1]
-key_f = sys.argv[2]
-data_f = sys.argv[3]
-sig_f = sys.argv[4]
-
-def generate_signature(key, data, sig_f):
-    print("Generating Signature")
-    h = SHA256.new(data)
-    rsa = RSA.importKey(key)
-    signer = PKCS1_v1_5.new(rsa)
-    signature = signer.sign(h)
-    with open(sig_f, 'wb') as f: f.write(signature)
-
-def verify_signature(key, data, sig_f):
-    print("Verifying Signature")
-    h = SHA256.new(data)
-    rsa = RSA.importKey(key)
-    signer = PKCS1_v1_5.new(rsa)
-    with open(sig_f, 'rb') as f: signature = f.read()
-    rsp = "Success" if (signer.verify(h, signature)) else " Verification Failure"
-    print(rsp)
-
-# Read all file contents
-with open(key_f, 'rb') as f: key = f.read()
-with open(data_f, 'rb') as f: data = f.read()
-
-if (op == "-s"):
-    # Generate Signature
-    generate_signature(key, data, sig_f)
-elif (op == "-v"):
-    # Verify Signature
-    verify_signature(key, data, sig_f)
-else:
-    #Error
-    usage()
+public class DigitalSignature {
+   public static void main(String args[]) throws Exception {
+      
+      Scanner sc = new Scanner(System.in);
+      System.out.println("Enter your message");
+      String msg = sc.nextLine();
+      
+      
+      KeyPairGenerator keyPairGen = KeyPairGenerator.getInstance("DSA");
+      
+      
+      keyPairGen.initialize(2048);
+      
+      
+      KeyPair pair = keyPairGen.generateKeyPair();
+      
+      
+      PrivateKey privKey = pair.getPrivate();
+      
+      
+      Signature sign = Signature.getInstance("SHA256withDSA");
+      
+      
+      sign.initSign(privKey);
+      byte[] bytes = "msg".getBytes();
+      
+      
+      sign.update(bytes);
+      
+      
+      byte[] signature = sign.sign();
+      
+      
+      System.out.println("Digital signature of the message: "+new String(signature, "UTF8"));
+   }
+}
